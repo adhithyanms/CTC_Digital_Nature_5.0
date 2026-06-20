869f827e2b03c6d87c112749efbc4d6712569ffa
adhithyanms <adhithyanshanmugam@gmail.com>
2026-06-20 10:38:44 +0530

    Week1-singleton and documentFactory

---
+++ week1/Design Patterns and Principles/Implementing the Factory Method Pattern/FactoryMethodPatternExample/src/PdfDocument.java
@@ -0,0 +1,10 @@
+package src;
+
+public class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening PDF document...");
+    }
+    public void save() {
+        System.out.println("Saving PDF document...");
+    }
+}
---
+++ week1/Design Patterns and Principles/Implementing the Factory Method Pattern/FactoryMethodPatternExample/src/WordDocument.java
@@ -0,0 +1,10 @@
+package src;
+
+public class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening Word document...");
+    }
+    public void save() {
+        System.out.println("Saving Word document...");
+    }
+}
